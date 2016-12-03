0373be8868acfb282ef9090093d597dad94c9e13
gregorydhughes <gregorydhughes@gmail.com>
2016-12-03 12:31:39 -0500

    Garry's silly request is gone.

--- QLearning/QValueRec.h
+++ QLearning/QValueRec.h
@@ -12,23 +12,14 @@
 // Details pertaining to each of the tiles in the room
 struct QValRec
 {
-	double rNorth;				// Q-Value for moving North
-	double rNorthEast;			// Q-Value for moving North East
-	double rEast;				// Q-Value for moving East
-	double rSouthEast;			// Q-Value for moving South East
-	double rSouth;				// Q-Value for moving South
-	double rSouthWest;			// Q-Value for moving South West
-	double rWest;				// Q-Value for moving rWest
-	double rNorthWest;			// Q-Value for moving North West
-
-	double qNorth;				// Q-Value for moving North
-	double qNorthEast;			// Q-Value for moving North East
-	double qEast;				// Q-Value for moving East
-	double qSouthEast;			// Q-Value for moving South East
-	double qSouth;				// Q-Value for moving South
-	double qSouthWest;			// Q-Value for moving South West
-	double qWest;				// Q-Value for moving rWest
-	double qNorthWest;			// Q-Value for moving North West
+	double rNorth;				// Reward Value for moving North
+	double rNorthEast;			// Reward Value for moving North East
+	double rEast;				// Reward Value for moving East
+	double rSouthEast;			// Reward Value for moving South East
+	double rSouth;				// Reward Value for moving South
+	double rSouthWest;			// Reward Value for moving South West
+	double rWest;				// Reward Value for moving rWest
+	double rNorthWest;			// Reward Value for moving North West
 };
 
 #endif // !TILEREC_H
