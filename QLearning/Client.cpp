bbb6ec9d13d353c2ee198f25335cbf538eebb7b2
GarryG <valepos@gmail.com>
2016-12-05 21:38:05 -0500

    asdf

--- QLearning/Client.cpp
+++ QLearning/Client.cpp
@@ -461,6 +461,6 @@ double getMaxQ(QValueRec *currState) {
 double calculateQLearnValue(double lWeight, double qVal, double qMax, double reward) {
 	double updatedQ = (ALPHA * lWeight) * (reward + (GAMMA * qMax) - qVal);
 	if (reward < 0.0)
-		return 0.0;
+		return -500;
 	return updatedQ;
 }
